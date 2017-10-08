dfcc4bb92c5f93366786748d0f934e86dc6e79e4
Yael Brown <yaelrbrown@gmail.com>
2017-10-08 19:13:53 -0400

    Initial Push
    Initial Push of Final project

---
+++ Crimes.java
@@ -0,0 +1,12 @@
+/*
+* File: Crimes.java
+* Author: Yael Brown
+* Date: October 8, 2017
+* Purpose: 
+*/
+
+package finalProj;
+
+public class Crimes {
+
+}
---
+++ TestCrimes.java
@@ -0,0 +1,17 @@
+/*
+* File: TestCrimes.java
+* Author: Yael Brown
+* Date: October 8, 2017
+* Purpose: Used to test methods in Crimes.java
+*/
+
+package finalProj;
+
+public class TestCrimes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
