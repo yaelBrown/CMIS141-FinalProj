9f7b2c7fbf52687b06b7b8dbe9cf58f76c3e7b06
Yael Brown <yaelrbrown@gmail.com>
2017-10-11 07:53:58 -0400

    update
    Able to read files now. Information is not parsed.

--- Crimes.java
+++ Crimes.java
@@ -1,5 +1,3 @@
-package finalProj;
-
 /*
 * File: Crimes.java
 * Author: Yael Brown
@@ -11,15 +9,16 @@ package finalProj;
 // Import Required libraries
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Crimes {
     // csv file read setup
-    static String fileName = "Crime.csv";
+	static Scanner scannerIn = null;
+    static FileInputStream in = null;   
     static BufferedReader inputStream = null;
-    inputStream = new BufferedReader(new FileReader(fileName));
     
     // Define variables
     static boolean check = false; // Used for while loop to keep reiterating try code. 
@@ -27,12 +26,46 @@ public class Crimes {
     static String selection = null;
 
     // Method for reading Crime.csv file
-    public static void read(){
-    	// some read code
-    }     
+    public static void readFile() throws IOException {
+    	int fileChar;
+    	String fileLine;
+    	in = new FileInputStream("Crime.csv");
+    	System.out.println("ReadIt File Contents");
+    	// Reads one char at a time
+    	while ((fileChar = in.read()) != -1) {
+    		// convert int to char
+    		System.out.print((char) fileChar);
+    	}
+    	// Separate the file output
+    	System.out.println("");
+    	System.out.println("Crime.csv File Contents using BufferedReader");
+    	// Use of Scanner and BufferedReader
+    	inputStream = new BufferedReader(new FileReader("Crime.csv"));
+    	scannerIn = new Scanner(inputStream);
+    	while (scannerIn.hasNext()) {
+    		if (scannerIn.hasNextInt()) {
+    			System.out.println(scannerIn.nextInt());
+    		}
+    		if (scannerIn.hasNextDouble()) {
+    			System.out.println(scannerIn.nextDouble());
+    		} else {
+    			scannerIn.next();
+    		}
+    	}
+    	// Separe the file output
+    	System.out.println("");
+    	// Use of
+    	inputStream = new BufferedReader(new FileReader("Crime.csv"));
+    	System.out.println("Crime.csv contents");
+    	// Read one Line using BufferedReader
+    	while ((fileLine = inputStream.readLine()) != null) {
+    		System.out.println(fileLine);
+    	}
+    }
+     
     
     // Method for printing menu
-    public static void menu() {
+    public static void menu() throws IOException {
         System.out.println("\n********** Welcome to the US Crime Statistical Application **************************\n");
         System.out.println("1. What were the percentages in population growth for each consecutive year from 1994 � 2013?");
         System.out.println("2. What year was the Murder rate the highest?");
@@ -48,7 +81,7 @@ public class Crimes {
         // Perform appropriate action with input
         switch (selection) {
             case "1": System.out.println("Your selection is 1");
-                      read();
+                      readFile();
                       menu();
                       break;
             default:  System.out.println("This is the default statement");
@@ -60,7 +93,11 @@ public class Crimes {
         System.out.println("\nThank you for trying the US Crimes Statistics Program.");
     } 
  
-    public static void main(String[] args) {
+    
+    
+    
+    
+    public static void main(String[] args) throws IOException {
   	   	try {
             menu();
             } catch (Exception e) {
